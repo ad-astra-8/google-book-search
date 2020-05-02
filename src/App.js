f721d0f563730acd077110d34120a7a74f2542d9
Wanda Antoine <wanda@thinkful.com>
2020-05-02 11:51:52 -0400

    fixed option bug

--- src/App.js
+++ src/App.js
@@ -1,227 +1,229 @@
 import React, { Component } from 'react'
 import './App.css'
-// import Search from './Search'
-// import Book from './Book'
+import Search from './Search'
+import Book from './Book'
 import Header from './Header'
 
 class App extends Component {
 
-    // //setup the constructor witht he default props and states
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         books: [],
-    //         error: null,
-    //         params: {
-    //             printType: '',
-    //             filter: '',
-    //             q: ''
-    //         }
-    //     }
-    // }
+    //setup the constructor witht he default props and states
+    constructor(props) {
+        super(props)
+        this.state = {
+            books: [],
+            error: null,
+            params: {
+                printType: '',
+                filter: '',
+                q: '',
+            }
+        }
+    }
 
-    // // convert query parameter from an object to a string
-    // formatQueryParams(params) {
-    //     const queryItems = Object.keys(params)
-    //         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    //     return queryItems.join('&')
-    // }
+    // convert query parameter from an object to a string
+    formatQueryParams(params) {
+        const queryItems = Object.keys(params)
+            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        return queryItems.join('&')
+    }
 
-    // // if an integer is empty, undefinded or null, default it to 0
-    // checkInteger(inputInteger) {
-    //     let outputValue = inputInteger
-    //     if (inputInteger === "") {
-    //         outputValue = 0
-    //     }
-    //     if (inputInteger === undefined) {
-    //         outputValue = 0
-    //     }
-    //     if (inputInteger == null) {
-    //         outputValue = 0
-    //     }
-    //     return outputValue
-    // }
+    // if an integer is empty, undefinded or null, default it to 0
+    checkInteger(inputInteger) {
+        let outputValue = inputInteger
+        if (inputInteger === "") {
+            outputValue = 0
+        }
+        if (inputInteger === undefined) {
+            outputValue = 0
+        }
+        if (inputInteger == null) {
+            outputValue = 0
+        }
+        return outputValue
+    }
 
-    // // if a string is undefinded or null, default it to "no details"
-    // checkString(inputString) {
-    //     let outputText = inputString
-    //     if (inputString === undefined) {
-    //         outputText = "no details"
-    //     }
-    //     if (inputString == null) {
-    //         outputText = "no details"
-    //     }
-    //     return outputText
-    // }
+    // if a string is undefinded or null, default it to "no details"
+    checkString(inputString) {
+        let outputText = inputString
+        if (inputString === undefined) {
+            outputText = "no details"
+        }
+        if (inputString == null) {
+            outputText = "no details"
+        }
+        return outputText
+    }
 
-    // // if a URL is undefinded or null, default it to the root url "/"
-    // checkURL(inputURL) {
-    //     let outputURL = inputURL
-    //     if (inputURL === undefined) {
-    //         outputURL = "/"
-    //     }
-    //     if (inputURL == null) {
-    //         outputURL = "/"
-    //     }
-    //     return outputURL
-    // }
+    // if a URL is undefinded or null, default it to the root url "/"
+    checkURL(inputURL) {
+        let outputURL = inputURL
+        if (inputURL === undefined) {
+            outputURL = "/"
+        }
+        if (inputURL == null) {
+            outputURL = "/"
+        }
+        return outputURL
+    }
 
-    // //get the imput from the user
-    // handleSearch = (e) => {
-    //     e.preventDefault()
+    //get the imput from the user
+    handleSearch = (e) => {
+        e.preventDefault()
 
-    //     //create an object to store the search filters
-    //     const data = {}
+        //create an object to store the search filters
+        const data = {}
 
-    //     //get all the from data from the form component
-    //     const formData = new FormData(e.target)
+        //get all the from data from the form component
+        const formData = new FormData(e.target)
 
-    //     //for each of the keys in form data populate it with form value
-    //     for (let value of formData) {
-    //         data[value[0]] = value[1]
-    //     }
+        //for each of the keys in form data populate it with form value
+        for (let value of formData) {
+            data[value[0]] = value[1]
+        }
 
-    //     //assigning the object from the form data to params in the state
-    //     this.setState({
-    //         params: data
-    //     })
+        //assigning the object from the form data to params in the state
+        this.setState({
+            params: data
+        })
 
-    //     //check if the state is populated with the search params data
-    //     console.log(this.state.params)
+        //check if the state is populated with the search params data
+        console.log(this.state.params)
 
-    //     //get the google books api url
-    //     const searchURL = 'https://www.googleapis.com/books/v1/volumes'
+        // const apiKey = 'AIzaSyDWUHuQdloTuZNxF_ydNs0fFM1tyWkiMAs';
 
-    //     //format the queryString paramters into an object
-    //     const queryString = this.formatQueryParams(data)
+        //get the google books api url
+        const searchURL = 'https://www.googleapis.com/books/v1/volumes'
 
-    //     //sent all the params to the final url
-    //     const url = searchURL + '?' + queryString
+        //format the queryString paramters into an object
+        const queryString = this.formatQueryParams(data)
 
-    //     console.log(url)
+        //sent all the params to the final url
+        const url = searchURL + '?' + queryString 
 
-    //     //define the API call parameters
-    //     const options = {
-    //         method: 'GET',
-    //         header: {
-    //             "Authorization": "",
-    //             "Content-Type": "application/json"
-    //         }
-    //     }
+        console.log(url)
 
-    //     //useing the url and paramters above make the api call
-    //     fetch(url, options)
+        // define the API call parameters
+        const options = {
+            method: 'GET',
+            header: {
+                "Authorization": "AIzaSyDgnEln73GAo0Wb13uabTNIgxXbdthI-uw",
+                "Content-Type": "application/json"
+            }
+        }
 
-    //         // if the api returns data ...
-    //         .then(res => {
-    //             if (!res.ok) {
-    //                 throw new Error('Something went wrong, please try again later.')
-    //             }
+        //useing the url and paramters above make the api call
+        fetch(url, options)
 
-    //             // ... convert it to json
-    //             return res.json()
-    //         })
+            // if the api returns data ...
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Something went wrong, please try again later.')
+                }
 
-    //         // use the json api output
-    //         .then(data => {
+                // ... convert it to json
+                return res.json()
+            })
 
-    //             //check if there is meaningfull data
-    //             console.log(data);
+            // use the json api output
+            .then(data => {
 
-    //             // check if there are no results
-    //             if (data.totalItems === 0) {
-    //                 throw new Error('No books found')
-    //             }
+                //check if there is meaningfull data
+                console.log(data);
 
-    //             // create and object with each one of the results
-    //             const aBooks = data.items.map(book => {
+                // check if there are no results
+                if (data.totalItems === 0) {
+                    throw new Error('No books found')
+                }
 
-    //                 // get the title, authors, description, imageLinks, previewLink from "volumeInfo"
-    //                 const { title, authors, description, imageLinks, previewLink } = book.volumeInfo
+                // create and object with each one of the results
+                const aBooks = data.items.map(book => {
 
-    //                 // get the saleability, retailPrice from "saleInfo"
-    //                 const { saleability, retailPrice } = book.saleInfo
+                    // get the title, authors, description, imageLinks, previewLink from "volumeInfo"
+                    const { title, authors, description, imageLinks, previewLink } = book.volumeInfo
 
-    //                 //if the image is not defined replace it with a no-image image
-    //                 let imageLinksOutput = ''
-    //                 if (imageLinks === undefined) {
-    //                     imageLinksOutput = 'https://legacytaylorsville.com/wp-content/uploads/2015/07/No-Image-Available1.png'
-    //                 } else {
-    //                     imageLinksOutput = imageLinks.thumbnail
-    //                 }
+                    // get the saleability, retailPrice from "saleInfo"
+                    const { saleability, retailPrice } = book.saleInfo
 
-    //                 //check if the data validation works
-    //                 console.log(this.checkString(title));
-    //                 console.log(this.checkString(authors));
-    //                 console.log(this.checkString(description));
-    //                 console.log(this.checkURL(previewLink));
-    //                 console.log(this.checkURL(imageLinksOutput));
-    //                 console.log(this.checkInteger(saleability));
-    //                 console.log(this.checkInteger(retailPrice));
+                    //if the image is not defined replace it with a no-image image
+                    let imageLinksOutput = ''
+                    if (imageLinks === undefined) {
+                        imageLinksOutput = 'https://legacytaylorsville.com/wp-content/uploads/2015/07/No-Image-Available1.png'
+                    } else {
+                        imageLinksOutput = imageLinks.thumbnail
+                    }
 
-    //                 // fix the inconsitent results and return them
-    //                 return {
-    //                     title: this.checkString(title),
-    //                     author: this.checkString(authors),
-    //                     description: this.checkString(description),
-    //                     previewLink: this.checkURL(previewLink),
-    //                     thumbnail_URL: this.checkURL(imageLinksOutput),
-    //                     saleability: this.checkInteger(saleability),
-    //                     price: this.checkInteger(retailPrice),
-    //                 }
-    //             })
+                    //check if the data validation works
+                    console.log(this.checkString(title));
+                    console.log(this.checkString(authors));
+                    console.log(this.checkString(description));
+                    console.log(this.checkURL(previewLink));
+                    console.log(this.checkURL(imageLinksOutput));
+                    console.log(this.checkInteger(saleability));
+                    console.log(this.checkInteger(retailPrice));
 
-    //             //check if the validated data is structured in a new array objects
-    //             console.log(aBooks);
+                    // fix the inconsitent results and return them
+                    return {
+                        title: this.checkString(title),
+                        author: this.checkString(authors),
+                        description: this.checkString(description),
+                        previewLink: this.checkURL(previewLink),
+                        thumbnail_URL: this.checkURL(imageLinksOutput),
+                        saleability: this.checkInteger(saleability),
+                        price: this.checkInteger(retailPrice),
+                    }
+                })
 
-    //             //send all the results to the state
-    //             this.setState({
-    //                 books: aBooks,
-    //                 error: null
-    //             })
-    //         })
+                //check if the validated data is structured in a new array objects
+                console.log(aBooks);
 
-    //         //catch connection errors
-    //         .catch(err => {
-    //             this.setState({
-    //                 error: err.message
-    //             })
-    //         })
+                //send all the results to the state
+                this.setState({
+                    books: aBooks,
+                    error: null
+                })
+            })
 
-    // }
+            //catch connection errors
+            .catch(err => {
+                this.setState({
+                    error: err.message
+                })
+            })
+
+    }
 
     render() {
 
-        // //if there is an error message display it
-        // const errorMessage = this.state.error ? <div>{this.state.error}</div> : false
+        //if there is an error message display it
+        const errorMessage = this.state.error ? <div>{this.state.error}</div> : false
 
-        // //get all the books from the state
-        // const library = this.state.books
+        //get all the books from the state
+        const library = this.state.books
 
-        // //map each book for the corresponding component
-        // const books = library.map((book, i) => {
-            // return 
-            // <Book
-            //     key={i}
-            //     title={book.title}
-            //     author={book.author}
-            //     description={book.description}
-            //     previewLink={book.previewLink}
-            //     thumbnail_URL={book.thumbnail_URL}
-            //     saleability={book.saleability}
-            //     price={book.price}
-            // />
-        // })
+        //map each book for the corresponding component
+        const books = library.map((book, i) => {
+            return <Book
+                key={i}
+                title={book.title}
+                author={book.author}
+                description={book.description}
+                previewLink={book.previewLink}
+                thumbnail_URL={book.thumbnail_URL}
+                saleability={book.saleability}
+                price={book.price}
+            />
+        })
 
 
         return (
-          <div className="App">
-              <Header />
-              {/* <Search handleSearch={this.handleSearch} /> */}
-              {/* {errorMessage}
-              {books} */}
-          </div>
-      )
+            <div className="App">
+                <Header />
+                <Search handleSearch={this.handleSearch} />
+                {errorMessage}
+                {books}
+                {/* <Book /> */}
+            </div>
+        )
     }
 }
 
--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,31 @@
-// return (
-//             <div className="App">
-//                 <Header />
-//                 <Search handleSearch={this.handleSearch} />
-//                 {errorMessage}
-//                 {books}
-//             </div>
-//         )
-//     }
-// }
+import React, { Component } from 'react';
 
-// export default App
+class Search extends Component {
+    render() {
+        return (
+            <div className="searchbook">
+                         <form onSubmit={this.props.handleSearch}>
+                    <label>Search: </label>
+                    <input type="text"  name='q' 
+                    className='search-bar' placeholder="henry" required />
+                    <button type="submit">Search</button>
+
+                    <div className="filter-bar">
+                        <label>Print Type</label>
+                        <select name="printType">
+                            <option value='all'>All</option>
+                        </select>
+                        <label>Book Type</label>
+                        <select name='filter'>
+                            <option value='ebooks'>ebooks</option>
+                        </select>
+
+                    </div>
+                </form>
+            </div>
+
+        )
+    }
+}
+
+export default Search;
