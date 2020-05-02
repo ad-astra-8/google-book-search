5ec0c292a5d05dd38c69de7ff4f25efa25e48925
Wanda Antoine <wanda@thinkful.com>
2020-05-01 21:25:30 -0400

    added components

---
+++ src/Search.js
@@ -0,0 +1,12 @@
+// return (
+//             <div className="App">
+//                 <Header />
+//                 <Search handleSearch={this.handleSearch} />
+//                 {errorMessage}
+//                 {books}
+//             </div>
+//         )
+//     }
+// }
+
+// export default App
